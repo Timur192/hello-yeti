08a0a939ef7a0aec3317417fa58c4c1ec2e319eb
Timur192 <ttimerbaev4@gmail.com>
2022-04-19 00:02:08 +0300

    Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Component from './Component';
+import AppBar from './AppBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <AppBar />
         <Component />
       </header>
     </div>
